80aa7e4d7bbf4ce0d0ccef4a7d15b5fe96f46ff0
Gabriel <gabrielrubinho05@gmail.com>
2023-08-15 00:05:12 -0300

    correções

--- implementacao.R
+++ implementacao.R
@@ -6,6 +6,8 @@
 #                                   2x + 3y <= 42
 #                                   3x + y <= 24
 #                                   x >= 0, y >= 0
+teste <- c(1, 1, 1, 5, 0)
+(min(teste) < 0)
 
 resultados <- c(18, 42, 24)
 matriz_restricoes <- matrix(c(2, 1, 2, 3, 3, 1), nrow = 3, byrow = TRUE)
@@ -55,7 +57,7 @@ metodo_simplex <- function(funcao_objetivo, matriz_restricoes,
       }
     }
     
-    # O código acima foi usado para montar as variáveis p1, p2, ..., baseada na quantidade de variável da função 
+    # O código acima foi usado para montar as variáveis p1, p2, ..., pN, baseada na quantidade de variável da função 
     
     diag(matriz[,c(paste0('P', seq(numero_coluna+1, ncol(matriz)-2)))]) <- 1
     
@@ -67,6 +69,189 @@ metodo_simplex <- function(funcao_objetivo, matriz_restricoes,
     for (i in 1:length(funcao_objetivo)){
       matriz[nrow(matriz), i+2] <- funcao_objetivo[i]
     }
+    
+    
+    valores_base <- c(0, 0, -funcao_objetivo, rep(0, numero_linha))
+    
+    # A PARTIR DAQUI, A MATRIZ ESTÁ MONTADA
+    
+    # Precisamos escolher o menor valor da última linha
+    # A partir daqui, começa a iteração
+    
+    
+    while((min(matriz[nrow(matriz),]) < 0) == TRUE) {
+      menor_valor <- min(matriz[nrow(matriz), ])
+      
+      for (i in 1:ncol(matriz)) {
+        if (matriz[nrow(matriz), i] == menor_valor){
+          vetor_coluna <- matriz[, i]
+          break
+        }
+      }
+      
+      
+      for (i in 1:ncol(matriz)) {
+        if(matriz[, i] == vetor_coluna) {
+          get <- matriz[,2]/vetor_coluna
+          salvar_coluna <- i
+          break
+        }
+      }
+
+      
+      get <- get[-length(get)]
+      
+      get <- get[get > 0]
+      
+      valor_linha <- min(get) 
+    
+      
+  
+      for (i in 1:nrow(matriz)) {
+        if (matriz[i, 2]/vetor_coluna[i] == valor_linha) {
+          linha_salva <- i
+        }
+        
+      }
+      
+      pivo <- matriz[linha_salva, salvar_coluna]
+      
+      matriz1 <- matriz
+      
+      matriz_teste <- matriz1
+      
+      for (i in 1:(ncol(matriz))) {
+        
+        matriz_teste[linha_salva, i] <- round(matriz_teste[linha_salva, i]/pivo, 2)
+      }
+    
+      # ACIMA, É PRA LINHA DO PIVO
+      
+      matriz_teste1 <- matriz_teste
+      
+      # BAIXO, DEMAIS LINHAS
+      # fazer uma do 0 pq a outra ta dando errado e eu n faço ideia do pq
+      for (i in 1:nrow(matriz_teste)) {
+        if (i == linha_salva) next 
+        
+        for (j in 1:ncol(matriz_teste)) {
+          value <- matriz_teste[i, j] - (matriz_teste[i, salvar_coluna]*matriz_teste[linha_salva, j])
+          matriz_teste1[i, j] <- value
+          
+        }
+        
+      }
+      
+      matriz_teste1[linha_salva, 1] <- valores_base[salvar_coluna]
+      
+      
+      matriz <- matriz_teste1
+  
+    }
+    
+    # O caso acima engloba, caso tenha mais de duas 
+    
+  }
+  
+  else
+    
+  {
+    stop('Erro na quantidade de linhas da matriz.')
+  }
+  
+  return(matriz)
+  print('Problema de maximização com os coeficientes: ')
+  
+}
+
+matriz_restricoes <- matrix(c(2, 1, 2, 3, 3, 1), nrow = 3, byrow = TRUE)
+metodo_simplex(c(3, 2), matriz_restricoes, c(18, 42, 24))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+metodo_simplex <- function(funcao_objetivo, matriz_restricoes, 
+                           resultados, maxi = TRUE) 
+{
+  # Como sempre irá passar os valores para o outro lado da equação, os valores da função
+  # Objetivo serão trocados de sinal
+  
+  for (i in 1:length(funcao_objetivo))
+  {
+    funcao_objetivo[i] <- -funcao_objetivo[i]
+  }
+  
+  # Precisamos saber a quantidade de linhas e colunas da matriz de restrições dada
+  
+  numero_linha  <- nrow(matriz_restricoes)
+  numero_coluna <- ncol(matriz_restricoes)
+  
+  # Só pode fazer se o número de linhas da matriz for igual ao número de resultados (b1)
+  
+  if (nrow(matriz_restricoes) == length(resultados)) 
+  {
+    
+    matriz <- matrix(ncol = 2 + numero_coluna + numero_linha, nrow = numero_linha + 2)
+    # Criação da matriz geral para casos
+    
+    colnames(matriz) <- c('Cb', 'Po', paste0('P', seq(1, ncol(matriz)-2))) 
+    rownames(matriz) <- c('Base', paste0('P', seq(numero_coluna + 1, ncol(matriz)- 2)), 'Z')
+    # Acima, alterei o nome das colunas para melhor visualização
+    
+    for (i in 1:nrow(matriz)-1) {
+      matriz[i+1, 1] <- 0
+      matriz[i+1, 2] <- resultados[i]
+      #matriz[i, 3] <- matriz_restricoes[i, 1]
+      #matriz[i, 4] <- matriz_restricoes[i, 2]
+    }
+    
+    # Agora, implementar baseado na quantidade de variáevis
+    
+    for (i in 1:numero_coluna) { # COMEÇA NA 3º COLUNA 
+      for (j in 1:numero_linha) {
+        matriz[j, i+2] <- matriz_restricoes[j, i] # tem um +3 na coluna, pois sempre começará da 3 
+      }
+    }
+    
+    # O código acima foi usado para montar as variáveis p1, p2, ..., baseada na quantidade de variável da função 
+    
+    diag(matriz[,c(paste0('P', seq(numero_coluna+1, ncol(matriz)-2)))]) <- 1
+    
+    # Na linha cima, substuímos todos os valores da diagonal restante pelas variáveis de "apoio"
+    
+    
+    matriz[is.na(matriz)] <- 0
+    
+    for (i in 1:length(funcao_objetivo)){
+      matriz[nrow(matriz), i+2] <- funcao_objetivo[i]
+    }
+    
+    valores_base <- -funcao_objetivo
+    
+    
+    
+    #matriz <- rbind(matriz, c(NA))
+    
+    # for (i in 1:length(funcao_objetivo)) {
+    #   matriz[nrow(matriz)+1, i+2] <- -funcao_objetivo[i]
+    #   
+    # }
     # A PARTIR DAQUI, A MATRIZ ESTÁ MONTADA
     
     # Precisamos escolher o menor valor da última linha
@@ -91,9 +276,9 @@ metodo_simplex <- function(funcao_objetivo, matriz_restricoes,
       }
       
       valor_linha <- min(get[get!=min(get)]) 
-    
       
-  
+      
+      
       for (i in 1:nrow(matriz)) {
         if (matriz[i, 2]/vetor_coluna[i] == valor_linha) {
           linha_salva <- i
@@ -135,7 +320,7 @@ metodo_simplex <- function(funcao_objetivo, matriz_restricoes,
       matriz <- matriz_teste1
       
       
-    }
+    } 
     
     # AQUI, acaba o loop.
     
@@ -159,13 +344,13 @@ metodo_simplex <- function(funcao_objetivo, matriz_restricoes,
     # }
     
     
-      
-      
     
     
     
     
-   
+    
+    
+    
     
     
     
@@ -181,18 +366,3 @@ metodo_simplex <- function(funcao_objetivo, matriz_restricoes,
   }
   
 }
-
-matrix(ncol = 2 + 3 + 2, nrow = 3+1)
-
-
-tamanho <- 5  # Tamanho da variável r (ajuste conforme necessário)
-r <- matrix(0, nrow = tamanho, ncol = tamanho)  # Crie uma matriz de zeros do tamanho especificado
-
-for (i in 1:tamanho) {
-  r[i, i] <- 1
-}
-
-# Imprima a matriz resultante
-print(r)
-teste <- c(1, 1, 2, 5, 2)
-min(teste)
